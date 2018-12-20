654dfb8abba0540b8f72ecca2eed9103c8b7a99f
Ahmed Ragab <ahmed.ragab@me.com>
2018-12-20 06:01:30 +0200

    fix

--- src/sticky-nav.directive.ts
+++ src/sticky-nav.directive.ts
@@ -1,56 +1,72 @@
-import { Directive, Input, Renderer, ElementRef, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Directive, Input, Renderer, ElementRef, OnInit } from "@angular/core";
+import { Observable } from "rxjs";
 
 @Directive({
-    selector: '[ngStickyNav]'
+	selector: "[ngStickyNav]"
 })
-
 export class StickyNavDirective implements OnInit {
-    private offsetTop: number;
-    private lastScroll: number = 0;
-    private isSticky: boolean = false;
-    @Input('stickyClass') stickyClass: string;
+	private offsetTop: number;
+	private lastScroll: number = 0;
+	private isSticky: boolean = false;
+	@Input("stickyClass") stickyClass: string;
 
-    constructor(private elementRef: ElementRef, private renderer: Renderer) {
+	constructor(private elementRef: ElementRef, private renderer: Renderer) { }
 
-    }
+	ngOnInit(): void {
+		this.offsetTop = this.elementRef.nativeElement.offsetTop;
 
-    ngOnInit(): void {
-        this.offsetTop = this.elementRef.nativeElement.offsetTop;
+		Observable.fromEvent(window, "scroll").subscribe(() =>
+			this.manageScrollEvent()
+		);
+	}
 
-        Observable.fromEvent(window, 'scroll').subscribe(() => this.manageScrollEvent());
-    }
+	private manageScrollEvent(): void {
+		const scroll = window.pageYOffset;
 
-    private manageScrollEvent(): void {
-        const scroll = window.pageYOffset;
+		if (
+			!this.isSticky &&
+			scroll > this.offsetTop
+		) {
+			this.setSticky();
+		} else if (
+			this.isSticky &&
+			scroll <= this.offsetTop
+		) {
+			this.unsetSticky();
+		}
+		this.lastScroll = scroll;
+	}
 
-        if (scroll > this.lastScroll && !this.isSticky && scroll >= this.offsetTop) {
-            this.setSticky();
-        } else if (scroll < this.lastScroll && this.isSticky && scroll <= this.offsetTop) {
-            this.unsetSticky();
-        }
-        this.lastScroll = scroll;
-    }
+	private setSticky(): void {
+		this.isSticky = true;
+		this.setStyle('position', 'fixed');
+		this.setStyle('will-change', 'top');
+		this.setStyle('top', '0');
+		this.setStyle("left", '0');
+		this.setStyle("right", '0');
+		this.setStyle("z-index", '1');
+		this.setClass(true);
+	}
 
-    private setSticky(): void {
-        this.isSticky = true;
-        this.setStyle('position', 'fixed');
-        this.setStyle('top', '0');
-        this.setClass(true);
-    }
+	private unsetSticky(): void {
+		this.isSticky = false;
+		this.setStyle("position", "static");
+		this.setClass(false);
+	}
 
-    private unsetSticky(): void {
-        this.isSticky = false;
-        this.setStyle('position', 'static');
-        this.setClass(false);
-    }
+	private setStyle(key: string, value: string): void {
+		this.renderer.setElementStyle(
+			this.elementRef.nativeElement,
+			key,
+			value
+		);
+	}
 
-    private setStyle(key: string, value: string): void {
-        this.renderer.setElementStyle(this.elementRef.nativeElement, key, value);
-    }
-
-    private setClass(add: boolean): void {
-        this.renderer.setElementClass(this.elementRef.nativeElement, this.stickyClass, add);
-    }
-
+	private setClass(add: boolean): void {
+		this.renderer.setElementClass(
+			this.elementRef.nativeElement,
+			this.stickyClass,
+			add
+		);
+	}
 }
